b2c2d9f46d9039528c6cf9606a8de89aee1de20a
Paul <pllrbt@gmail.com>
2023-09-04 12:32:16 -0400

    Update playback.js

--- playback.js
+++ playback.js
@@ -12,26 +12,26 @@ WF.trackEntry = {};
 
 WF.call.enterBR = function () {
 
-     WF.trackEntry = WF.sparky.animationState.setAnimation(0, "bottomRight", false);
+     WF.trackEntry = WF.sparky.animationState.setAnimation(0, "enter/bottomRight", false);
 
 };
 
 WF.call.idleStart = function (loopBool) {
 
     if (!loopBool) loopBool = true;
-    WF.trackEntry = WF.sparky.animationState.setAnimation(0, "idle", loopBool);
+    WF.trackEntry = WF.sparky.animationState.setAnimation(0, "idles/idle", loopBool);
 
 };
 
 WF.call.charge = function (valNum) {
 
     if (!valNum) valNum = 0;
-    WF.trackEntry = WF.sparky.animationState.setAnimation(10, "charge-" + valNum, false);
+    WF.trackEntry = WF.sparky.animationState.setAnimation(10, "gauge/charge-" + valNum, false);
 
 };
 
 WF.call.wag = function () {
 
-    WF.trackEntry = WF.sparky.animationState.setAnimation(9, "wag", true);
+    WF.trackEntry = WF.sparky.animationState.setAnimation(9, "idles/wag", true);
 
 };
