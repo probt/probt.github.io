053504962c967212c701e38334ad4ae49ed3ba85
Paul <pllrbt@gmail.com>
2023-09-04 12:19:24 -0400

    new anims

--- playback.js
+++ playback.js
@@ -10,8 +10,28 @@
 WF.call = {};
 WF.trackEntry = {};
 
-WF.call.idleStart = function () {
+WF.call.enterBR = function () {
 
-    WF.trackEntry = WF.sparky.animationState.setAnimation(0, "idle", true);
+     WF.trackEntry = WF.sparky.animationState.setAnimation(0, "bottomRight", false);
+
+};
+
+WF.call.idleStart = function (loopBool) {
+
+    if (!loopBool) loopBool = true;
+    WF.trackEntry = WF.sparky.animationState.setAnimation(0, "idle", loopBool);
+
+};
+
+WF.call.charge = function (valNum) {
+
+    if (!valNum) valNum = 0;
+    WF.trackEntry = WF.sparky.animationState.setAnimation(10, "charge-" + valNum, false);
+
+};
+
+WF.call.wag = function () {
+
+    WF.trackEntry = WF.sparky.animationState.setAnimation(9, "wag", true);
 
 };
