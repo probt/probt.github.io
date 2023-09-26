ebee03e508cd55e24b1fe3357a8c497372833279
Paul <pllrbt@gmail.com>
2023-09-26 08:53:51 -0400

    Update keys.js

--- sparky-scene/js/keys.js
+++ sparky-scene/js/keys.js
@@ -6,18 +6,46 @@
  * @version      1.0.0
  */
 
+console.log ("a = leftIn / s = leftOut / f = rightIn / d = rightOut");
 
 window.onkeydown = function (e) {
     var code = e.keyCode ? e.keyCode : e.which;
-    if (code === 38) { //up key
-        console.log('up');
-    } else if (code === 40) { //down key
-        console.log('down');
-    } else {
-        console.log(code);
+    if (code === 65) {        //a
+        WF.call.keyEvents("leftIn");
+    } else if (code === 83) { //s
+        WF.call.keyEvents("leftOut");
+    } else if (code === 70) { //f
+        WF.call.keyEvents("rightIn");
+    } else if (code === 68) { //d
+        WF.call.keyEvents("rightOut");
     }
 };
 
+WF.call.keyEvents = function (action) {
+
+    if (action === "leftIn") {        //a
+        WF.sparky.animationState.setDefaultMix(0);
+        WF.sparky.animationState.setAnimation(0, "setup/midLeftSmall", false);
+        WF.sparky.animationState.setDefaultMix(0.5);
+        WF.sparky.animationState.addAnimation(0, "enter/midLeftSmall", false, 1);
+        WF.sparky.animationState.addAnimation(0, "idles/midLeftSmall", true, 0);
+        
+    } else if (action === "leftOut") { //s
+        WF.sparky.animationState.setAnimation(0, "exit/midLeftSmall", false);
+        
+    } else if (action === "rightIn") { //f
+        WF.sparky.animationState.setDefaultMix(0);
+        WF.sparky.animationState.setAnimation(0, "setup/bottomRight", false);
+        WF.sparky.animationState.setDefaultMix(0.5);
+        WF.sparky.animationState.addAnimation(0, "enter/bottomRight", false, 1);
+        WF.sparky.animationState.addAnimation(0, "idles/bottomRight", true, 0);
+        
+    } else if (action === "rightOut") { //d
+        WF.sparky.animationState.setAnimation(0, "exit/bottomRight", false);
+        
+    }
+
+}
 
 // WF.call = {};
 
