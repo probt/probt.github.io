89e8df6c7204f957ba95c43b21494a8befece18e
Paul <pllrbt@gmail.com>
2023-09-26 09:30:36 -0400

    Update keys.js

--- sparky-character/js/keys.js
+++ sparky-character/js/keys.js
@@ -3,25 +3,25 @@
  * @copyright    2023 Waterford.org
  *
  * @file         Spine: Key events for animation triggers
- * @version      0.9.2
+ * @version      1.0.0
  */
 
-console.log ("a = leftIn / s = leftOut / f = rightIn / d = rightOut");
+console.log ("up = lookUp / down = lookDown / b = blink / i = idleToggle / w = wagToggle / r = ringsToggle");
 
 window.onkeydown = function (e) {
     var code = e.keyCode ? e.keyCode : e.which;
-    console.log(code);
-    // if (code === 65) {        //a
-    //     WF.call.blink();
-    // } else if (code === 83) { //s
-    //     WF.call.headLookUp();
-    // } else if (code === 70) { //f
-    //     WF.call.headLookDown();
-    // } else if (code === 68) { //d
-    //     WF.call.bodyIdle();
-    // } else if (code === 68) { //d
-    //     WF.call.tailWag();
-    // } else if (code === 68) { //d
-    //     WF.call.rings();
-    // } 
+    //console.log(code);
+    if (code === 66) {        //b
+        WF.call.blink();
+    } else if (code === 38) { //up
+        WF.call.headLookUp();
+    } else if (code === 40) { //down
+        WF.call.headLookDown();
+    } else if (code === 73) { //i
+        WF.call.bodyIdle();
+    } else if (code === 87) { //w
+        WF.call.tailWag();
+    } else if (code === 82) { //r
+        WF.call.rings();
+    } 
 };
