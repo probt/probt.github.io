1286fab5aa0c9021d27f978f9b0c5333e9d6835b
Paul <pllrbt@gmail.com>
2025-06-12 10:52:02 -0400

    anim names

--- print-concepts-crane/js/playback.js
+++ print-concepts-crane/js/playback.js
@@ -17,36 +17,36 @@ const trackCrane = 0;
 
 WF.call.idle = function () {
 
-    WF.crane.animationState.setAnimation(trackCrane, "enter", false, 0);
+    WF.crane.animationState.setAnimation(trackCrane, "1-enter", false, 0);
 
 };
 
 WF.call.enter = function () {
 
-    WF.crane.animationState.setAnimation(trackCrane, "idle", false, 0);
+    WF.crane.animationState.setAnimation(trackCrane, "2-idle", false, 0);
 
 };
 
 WF.call.hookDrop = function () {
 
-    WF.crane.animationState.addAnimation(trackCrane, "hookDrop", false, 0);
+    WF.crane.animationState.addAnimation(trackCrane, "3-hookDrop", false, 0);
 
 };
 
 WF.call.hookRaiseOne = function () {
 
-    WF.crane.animationState.addAnimation(trackCrane, "hookRaise-1", false, 0);
+    WF.crane.animationState.addAnimation(trackCrane, "4-hookRaise-1", false, 0);
 
 };
 
 WF.call.enterTwo = function () {
 
-    WF.crane.animationState.addAnimation(trackCrane, "enter-2", false, 0);
+    WF.crane.animationState.addAnimation(trackCrane, "5-enter-2", false, 0);
 
 };
 
 WF.call.hookRaiseTwo = function () {
 
-    WF.crane.animationState.addAnimation(trackCrane, "hookRaise-2", false, 0);
+    WF.crane.animationState.addAnimation(trackCrane, "6-hookRaise-2", false, 0);
 
 };
